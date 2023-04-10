60584027d5b4acc92a8ab7024d9adff6703320aa
sweetim <hosweetim@gmail.com>
2023-04-11 00:28:21 +0900

    update page meta

--- src/app/counter/layout.tsx
+++ src/app/counter/layout.tsx
@@ -1,7 +1,34 @@
 import "@near-wallet-selector/modal-ui/styles.css";
 
+import { Metadata } from "next";
+
 import { NavBar } from "@/app/modules/counter/components"
 import { WalletSelectorContextProvider } from "@/app/context/WalletSelectorContext";
+
+export const metadata: Metadata = {
+    title: "counter",
+    description: "counter - near dapp",
+    icons: {
+        icon: {
+            url: "/counter.svg",
+            type: "image/svg"
+        }
+    },
+    openGraph: {
+        title: "counter",
+        description: "counter - near dapp",
+        url: "https://timx.co/counter",
+        siteName: "counter",
+        images: [
+            {
+                url: "https://timx.co/counter.svg",
+                width: 64,
+                height: 64,
+            },
+        ],
+        locale: "en-US",
+    },
+}
 
 export default function RootLayout({
     children,
