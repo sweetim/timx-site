e8abbc4b18b11f242086daa07b19041cda537464
sweetim <hosweetim@gmail.com>
2023-04-11 00:29:07 +0900

    updated nav bar

--- src/app/modules/counter/components/NavBar.tsx
+++ src/app/modules/counter/components/NavBar.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { FC, useEffect, useState } from "react"
-import { UserCircleIcon, XMarkIcon } from "@heroicons/react/24/solid"
+import { FC, useEffect, useState, useRef } from "react"
+import { ChevronDownIcon, CheckCircleIcon } from "@heroicons/react/24/solid"
 
-import { CounterLogo, SignOutIcon } from "@/app/icons";
-import ChainDropDownMenu from "./ChainDropDownMenu";
+import { CounterLogo, NearLogo, SignOutIcon } from "@/app/icons";
 import { useWalletSelector } from "@/app/context/WalletSelectorContext";
 
+import { Drawer, DrawerOptions } from "flowbite"
 
 const NavBar: FC = () => {
     const { selector, modal, accountId } = useWalletSelector();
@@ -23,12 +23,92 @@ const NavBar: FC = () => {
         await wallet.signOut()
     }
 
+    const [supportedChains, setSupportedChains] = useState([
+        {
+            name: "mainnet",
+            logo: <NearLogo className="mr-2 fill-white w-4" />,
+            isSelected: false
+        },
+        {
+            name: "testnet",
+            logo: <NearLogo className="mr-2 fill-pink-600 w-4" />,
+            isSelected: true
+        }
+    ])
+
+    function selectedChainHandler(chainName: string) {
+        setSupportedChains(prev => {
+            return prev.map((item) => {
+                item.isSelected = item.name === chainName
+                return item
+            })
+        })
+
+        chainDrawer?.hide()
+    }
+
+    const renderSupportedChains = supportedChains
+        .map(chain => {
+            return (
+                <li key={chain.name} className="hover:bg-gray-600 hover:text-white">
+                    <button type="button" className="w-full" onClick={ev => selectedChainHandler(chain.name)}>
+                        <div className="flex flex-row items-center p-4">
+                            {chain.logo}
+                            <span className="flex-1 flex justify-start">
+                                {chain.name}
+                            </span>
+                            {chain.isSelected && <CheckCircleIcon className="fill-slate-300 w-7 h-7" />}
+                        </div>
+                    </button>
+                </li>
+            )
+        })
+
+    const renderSelectedChainLogo = supportedChains
+        .filter(({ isSelected }) => isSelected)
+        .pop()
+        ?.logo
+
+    const chainDrawerRef = useRef(null)
+    const [chainDrawer, setChainDrawer] = useState<Drawer | null>(null)
+
+    useEffect(() => {
+        console.log(chainDrawerRef.current)
+        const options: DrawerOptions = {
+            placement: "bottom",
+            backdrop: true,
+            bodyScrolling: false,
+            edge: false,
+        }
+        setChainDrawer(new Drawer(chainDrawerRef.current, options))
+    }, [chainDrawerRef])
+
+    function chainDrawerClickHandler() {
+        chainDrawer?.show()
+    }
 
     return (
         <nav className="flex justify-between items-center p-3">
             <CounterLogo className="w-8 fill-white" />
+
             <div className="flex items-center">
-                <ChainDropDownMenu />
+                <button onClick={chainDrawerClickHandler}
+                    className="mr-2 text-white rounded-lg text-sm px-2 py-2 text-center inline-flex items-center hover:bg-slate-700"
+                    type="button">
+                    {renderSelectedChainLogo}
+                    <ChevronDownIcon className="w-4 h-4" />
+                </button>
+
+                <div ref={chainDrawerRef}
+                    className="border-t border-blue-300 translate-y-full fixed bottom-0 left-0 z-40 w-full overflow-y-auto transition-transform bg-zinc-900"
+                    tabIndex={1}
+                    aria-labelledby="chain-drawer-bottom-label">
+                    <ul
+                        className="text-lg text-gray-200"
+                        aria-labelledby="chain-drawer-drop-down">
+                        { renderSupportedChains }
+                    </ul>
+                </div>
 
                 {!isConnected && <button
                     className="text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center ml-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
@@ -44,13 +124,11 @@ const NavBar: FC = () => {
                     data-drawer-placement="bottom"
                     aria-controls="profile-drawer">
                     <span className="font-medium text-white">{accountIdShortForm}</span>
-                    {/* {!!!accountIdShortForm && <UserCircleIcon className="h-10"/>} */}
                 </button>}
 
                 <div id="profile-drawer"
-                    className="fixed -translate-x-full bottom-0 left-0 right-0 z-40 w-full h-[calc(100vh-64px)] p-4 overflow-y-auto transition-transform bg-gray-900"
-                    tabIndex={-1}
-                    aria-hidden="true"
+                    className="border-t border-blue-300 translate-y-full fixed bottom-0 left-0 z-40 w-full h-[calc(100vh-64px)] p-4 overflow-y-auto transition-transform bg-zinc-900"
+                    tabIndex={1}
                     aria-labelledby="drawer-bottom-label">
 
                     <div className="flex flex-row">
@@ -58,7 +136,6 @@ const NavBar: FC = () => {
                             className="inline-flex items-center mb-4 text-base font-semibold text-gray-400">
                             <div className="mr-2 relative inline-flex items-center justify-center w-10 h-10 overflow-hidden rounded-full bg-gray-600">
                                 <span className="font-medium text-white">{accountIdShortForm}</span>
-                                {/* {!!!accountIdShortForm && <UserCircleIcon className="h-10"/>} */}
                             </div>
                             {accountId}
                         </h5>
--- src/app/modules/counter/components/index.ts
+++ src/app/modules/counter/components/index.ts
@@ -1,4 +1,3 @@
 export { default as ActionCounter } from "./ActionCounter"
 export { default as NavBar } from "./NavBar"
 export { default as Timeline } from "./Timeline"
-export { default as ChainDropDownMenu } from "./ChainDropDownMenu"
