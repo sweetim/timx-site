3535e36a16cab518890d838e26ce94e7247682ad
sweetim <hosweetim@gmail.com>
2024-04-20 22:12:04 +0900

    added post api

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,9 @@
 export async function GET(request: Request) {
   return new Response('Hello, Next.js!')
 }
+
+export async function POST(request: Request) {
+  const data = await request.json()
+  console.log(data)
+  return new Response('Hello, Next.js POST!')
+}
