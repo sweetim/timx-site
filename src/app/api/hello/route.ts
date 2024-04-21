a4cf12390b3d0bcdaded1c5954c9319295b17384
sweetim <hosweetim@gmail.com>
2024-04-21 10:26:37 +0900

    updated to use env var

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,11 +2,11 @@ import { Realtime } from "ably"
 
 const {
   ABLY_API_KEY,
+  APTOS_COUNTER_MODULE_ADDRESS,
 } = process.env
 
 const ABLY_APTOS_COUNTER_CHANNEL_NAME = "aptos-counter"
 
-const APTOS_COUNTER_MODULE_ADDRESS = "0x6b2cf48e40e3b651c309dc444d0d094ed9c342089289c8bf50c4a5646271f20b"
 const APTOS_COUNTER_RECORD_EVENT_NAME = `${APTOS_COUNTER_MODULE_ADDRESS}::counter::CounterRecordEvent`
 
 const ably = new Realtime(ABLY_API_KEY!)
@@ -22,9 +22,11 @@ export async function POST(request: Request) {
 
   const ev = data.events
       .filter(({ type }: any) => type === APTOS_COUNTER_RECORD_EVENT_NAME)
-      .map(({ data }: any) => data);
+      .map(({ data }: any) => data)
+      .flat();
 
   console.log(ev)
+
   await channel.publish({
     data: ev
   })
